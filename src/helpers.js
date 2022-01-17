71a90e3228beb64fba305e9ec930ca5ca55b318c
Adophilus <uchenna19of@gmail.com>
2022-01-17 06:09:41 +0100

    checkpointing...

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,41 @@
+const axios = require("axios");
 const fs = require("fs");
+const path = require("path");
+
+function loadConfig (configPath = "./.env") {
+  return JSON.parse(fs.readFileSync(configPath));
+}
+
+async function fetchABI (contractAddress, network, config) {
+  let apiKey, apiUrl;
+
+  if (network === "polygon") {
+    apiKey = config.explorers.polygonscan.api.key;
+    apiUrl = config.explorers.polygonscan.api.url;
+  }
+  else {
+    throw new Error("Unsupported or invalid network!");
+  }
+
+  let res = await axios.get(apiUrl, {
+    params: {
+      module: "contract",
+      action: "getabi",
+      address: contractAddress,
+      format: "raw",
+      apikey: apiKey
+    }
+  });
+
+  return res.data;
+}
+
+function loadABI (abi) {
+  return JSON.parse(fs.readFileSync(path.join("abis", `${abi}.abi`)));
+}
 
 module.exports = {
-  loadConfig (configPath = ".env") {
-    return JSON.parse(fs.readFileSync(configPath));
-  }
+  loadConfig,
+  fetchABI,
+  loadABI
 }
--- web3-polygon-test/index.js
+++ web3-polygon-test/index.js
@@ -1,26 +1,35 @@
-const axios = require("axios");
 const Web3 = require("web3");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const helpers = require("../src/helpers");
 
 const config = helpers.loadConfig();
 
-let provider = new Web3.providers.HttpProvider(config.networks.polygon.mainnet.moralis);
+let provider = new HDWalletProvider(config.wallets.nft.privateKey, config.networks.polygon.mainnet.moralis);
+// let provider = new Web3.providers.HttpProvider(config.networks.polygon.mainnet.moralis);
 const web3 = new Web3(provider);
 let accounts = {};
 let openstoreContract;
 
 async function setup () {
-  let res = await axios.get("http://api.polygonscan.com/api", {params: {
-    module: "contract",
-    action: "getabi",
-    address: config.contracts.openstore.address,
-    format: "raw",
-    apikey: config.explorers.polygonscan.apiKey
-  }});
+  let openstoreABI;
 
-  let openstoreAbi = res.data;
-  openstoreContract = new web3.eth.Contract(openstoreAbi, config.contracts.openstore.address);
+  try {
+    // openstoreABI = await helpers.fetchABI(config.contracts.openstore.address, "polygon", config);
+    openstoreABI = helpers.loadABI("openstore");
+  }
+  catch (err) {
+    console.log("An error occurred while fetching the openstore ABI");
+    console.log(err);
+    process.exit(1);
+  }
+
+  openstoreContract = new web3.eth.Contract(openstoreABI, config.contracts.openstore.address);
   accounts.nft = web3.eth.accounts.wallet.add(config.wallets.nft.privateKey);
+}
+
+async function interim () {
+  console.log(await web3.eth.getAccounts());
+  process.exit();
 }
 
 async function main () {
@@ -28,29 +37,59 @@ async function main () {
 
   let wallet = "nft";
   let nft = "BillionaireNFT";
+  let res;
 
   // console.log(openstoreContract.methods); // openstore smart contract methods
 
-  openstoreContract.methods.setApprovalForAll(config.wallets[wallet].address, true)
-    .call()
-    .catch(err => console.log(`An error occurred while calling setApprovalForAll.\n${err}`));
+  await interim();
 
-  openstoreContract.methods.totalSupply(config.tokens.erc1155[nft])
-    .call()
-    .then(res => console.log(`The total supply of ${nft} is ${res}`));
+  try {
+    await openstoreContract.methods.setApprovalForAll(config.wallets[wallet].address, true)
+      .call();
+  }
+  catch (err) {
+    console.log("An error occurred while calling setApprovalForAll");
+    console.log(err);
+  }
 
-  openstoreContract.methods.balanceOf(config.wallets[wallet].address, config.tokens.erc1155[nft])
-    .call()
-    .then(res => console.log(`${wallet} account has a total of ${res} ${nft} tokens`));
+  try {
+    res = await openstoreContract.methods.totalSupply(config.tokens.erc1155[nft])
+      .call();
+    console.log(`The total supply of ${nft} is ${res}`);
+  }
+  catch (err) {
+    console.log(`An error occurred while trying to get the total supply of ${nft} tokens`);
+    if (err.response) {
+      console.log(err.response);
+    }
+    else {
+      console.log(err.message);
+    }
+  }
+
+  try {
+    res = await openstoreContract.methods.balanceOf(config.wallets[wallet].address, config.tokens.erc1155[nft])
+      .call();
+    console.log(`${wallet} account has a total of ${res} ${nft} tokens`);
+  }
+  catch (err) {
+    console.log(`An error occurred while trying to get the balance of ${nft} tokens in ${wallet} account`);
+    console.log(err);
+  }
 
   let safeTransferFromCall = await openstoreContract.methods.safeTransferFrom(accounts[wallet].address, config.wallets.test.address, config.tokens.erc1155[nft], 1, 0x00);
   // let functionSignature = accounts[wallet].sign(safeTransferFromCall.encodeABI());
   let functionSignature = web3.eth.accounts.sign(safeTransferFromCall.encodeABI(), accounts[wallet].privateKey);
 
-  openstoreContract.methods.executeMetaTransaction(accounts[wallet].address, functionSignature.message, functionSignature.r, functionSignature.s, functionSignature.v)
-    .call()
-    .then(res => console.log(res))
-    .catch(err => console.log(`An error occurred while transferring ${nft}.\n${err}`))
+  try {
+    let res = await openstoreContract.methods.executeMetaTransaction(accounts[wallet].address, functionSignature.message, functionSignature.r, functionSignature.s, functionSignature.v)
+      .call({ from: accounts[wallet].address })
+    console.log(res);
+  }
+  catch (err) {
+    console.log(`An error occurred while transferring ${nft}`);
+    console.log(err.message);
+  }
 }
 
 main();
