54a2dad703b4bd9b5e586dbe6615d96a9f52bb0c
Adophilus <uchenna19of@gmail.com>
2022-02-10 19:03:53 +0100

    debugging the the hostel smart contract

--- contracts/Hostel.sol
+++ contracts/Hostel.sol
@@ -4,6 +4,7 @@ contract Hostel {
   struct Tenant {
     string first_name;
     string last_name;
+    address addr;
   }
 
   struct Room {
@@ -11,6 +12,7 @@ contract Hostel {
     string name;
     uint rent;
     Tenant occupant;
+    bool is_vacant;
   }
 
   struct OccupancyInfo {
@@ -29,27 +31,34 @@ contract Hostel {
   // mapping(uint => Room) public Room_by_number;
   // mapping(uint => OccupancyInfo) private occupancies;
 
+  constructor (uint _a) {
+    for (uint8 i = 0; i < 10; i++) {
+      _createRoom(i);
+    }
+  }
+
   modifier onlyLandlord () {
     require(msg.sender == landlord, "Only the landlord can access this method!");
     _;
   }
 
   modifier notLandlord () {
-    require(msg.sender !== landlord, "Only tenants can access this method!");
+    require(msg.sender != landlord, "Only tenants can access this method!");
     _;
   }
 
   modifier onlyWhileVacant (uint8 _room_id) {
-    require(rooms[_room_id].is_vacant, "This room is occupied!");
+    require(!rooms[_room_id].is_vacant, "This room is occupied!");
     _;
   }
 
   modifier enoughRent (uint8 _room_id) {
     require(msg.value >= rooms[_room_id].rent, "Not enough rent!");
+    _;
   }
 
   modifier sameTenant (uint8 _room_id) {
-    require(msg.sender === rooms[_room_id].tenant.addr, "Only the occupant is allowed in here!");
+    require(msg.sender == rooms[_room_id].tenant.addr, "Only the occupant is allowed in here!");
     _;
   }
 
@@ -58,13 +67,18 @@ contract Hostel {
     _;
   }
 
-  function setLandlord (address _landlord) payable {
+  function _createRoom (uint8 _id) internal {
+    rooms.push(Room(_id, "room", false, false));
+  }
+
+  function setLandlord (address _landlord) public payable {
     if (!landlord) {
       landlord = _landlord;
     }
   }
 
-  function inspectRoom (uint8 _room_id) onlyWhileVacant returns Room {
-    return rooms[_room_id];
+  function inspectRoom (uint8 _room_id) onlyWhileVacant public {
+    Room memory room = rooms[_room_id];
+    return (room.number, room.name, room.rent);
   }
 }
